ec7048b31d5aa1549ba58fb5f79d584e73327d75
Awank Kaley <awank.kaley.10@gmail.com>
2019-09-24 19:53:57 +0700

    22/9

--- Vieweoq.py
+++ Vieweoq.py
@@ -48,13 +48,13 @@ class Plot:
         self.labelongkir = Label(self.master, text="Biaya Pesan (Rp) : ", anchor=E, justify=LEFT)
         self.labelongkir.grid(row=6, column=0)
         self.ongkir_id = StringVar()
-        self.ongkir = Entry(self.master,textvariable=self.ongkir_id, width=10)
+        self.ongkir = Entry(self.master, textvariable=self.ongkir_id, width=10)
         self.ongkir.grid(row=6, column=1)
 
         self.labelgudang = Label(self.master, text="Biaya Gudang (%) : ", anchor=E, justify=LEFT)
         self.labelgudang.grid(row=7, column=0)
-        self.gudang_id = StringVar()
-        self.gudang = Entry(self.master,textvariable=self.gudang_id, width=10)
+        self.gudang_id = IntVar()
+        self.gudang = Entry(self.master, textvariable=self.gudang_id, width=10)
         self.gudang.grid(row=7, column=1)
 
         self.labelharikerja = Label(self.master, text="Hari Kerja (Hari) : ", anchor=E, justify=LEFT)
@@ -67,7 +67,8 @@ class Plot:
         self.leadtime = Entry(self.master, width=10)
         self.leadtime.grid(row=6, column=3)
 
-        self.tombol_proses = Button(self.master, text="Proses", command=lambda: self.proses(data), bg='green', width='10')
+        self.tombol_proses = Button(self.master, text="Proses", command=lambda: self.proses(data), bg='green',
+                                    width='10')
         self.tombol_proses.grid(row=7, column=3)
 
         self._separator = ttk.Separator(self.master, orient="horizontal")
@@ -79,30 +80,31 @@ class Plot:
         if not file_name:
             return
         hasil = pd.read_excel(file_name)
-        if len(hasil.MOQ) == 0 :
+        if len(hasil.MOQ) == 0:
             tkMessageBox.showerror('Peringatan', 'Data Tidak Mencukupi !')
         self.path = file_name
         self.adaFile["text"] = file_name
 
-    def proses(self,data):
+    def proses(self, data):
+        dataku = data
         if self.adaFile["text"] == "File belum ada":
             tkMessageBox.showerror("Perhatian", "Belum Ada Data  !")
         else:
             hasil = pd.read_excel(self.path)
             MOQ = hasil.MOQ.values.tolist()
             harga = hasil.Harga.values.tolist()
-            def eoq_awal(data):
-                EOQAWAL = math.sqrt((2 * data * S) / (harga[0] * self.gudang_id))
+
+            def eoq_awal():
+                EOQAWAL = math.sqrt((2 * dataku * S) / (harga[0] * self.gudang_id))
                 hasil = [round(EOQAWAL)]
                 for n in range(1, len(MOQ)):
                     hasil.append(MOQ[n])
                 print(hasil)
-            eoq_awal(data)
+
+            eoq_awal()
+
 
 def main(data):
     root_window = Tk()
     program = Plot(root_window, data)
     root_window.mainloop()
-
-if __name__ == '__main__':
-    main(9050)
