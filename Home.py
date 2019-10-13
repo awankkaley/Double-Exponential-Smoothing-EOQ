d0d2572b51c89f25828a7f670c59ae1f4329e3a4
Awank Kaley <awank.kaley.10@gmail.com>
2019-10-13 16:40:07 +0700

    Fixing All

--- Home.py
+++ Home.py
@@ -11,22 +11,45 @@ except ImportError:
 from Ramalan import Ramalan
 from History import History
 from LihatData import LhatData
+
 LARGE_FONT = ("Verdana", 12)
 
 
 class Utama(Tk):
-
     def __init__(self, *args, **kwargs):
         Tk.__init__(self, *args, **kwargs)
         container = Frame(self)
 
-        container.pack(side="top", fill="both", expand=True)
+        self.menubar = Menu(container)
+        self.config(menu=self.menubar)
+        self.title("WIRAWAN PRIMA KALEY")
+        self.fileMenu = Menu(self.menubar)
+        self.fileMenu.add_command(label="Import Baru", command=lambda: self.show_frame(Ramalan, container))
+        self.menubar.add_cascade(label="Import", menu=self.fileMenu)
+        self.hisMenu = Menu(self.menubar)
+        self.hisMenu.add_command(label="Proses Data", command=lambda: self.show_frame(History, container))
+        self.menubar.add_cascade(label="History", menu=self.hisMenu)
+        self.lhtMenu = Menu(self.menubar)
+        self.lhtMenu.add_command(label="Lihat Data", command=lambda: self.show_frame(LhatData, container))
+        self.menubar.add_cascade(label="Lihat Data", menu=self.lhtMenu)
+        self.exit = Menu(self.menubar)
+        self.exit.add_command(label="Keluar", command=lambda: self.quit())
+        self.menubar.add_cascade(label="Keluar", menu=self.exit)
 
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
 
         self.frames = {}
+        self.recycle(container)
+        self.show_frame(Ramalan, container)
 
+    def show_frame(self, cont, container):
+        self.recycle(container)
+        frame = self.frames[cont]
+        container.pack(side="top", fill="both", expand=True)
+        frame.tkraise()
+
+    def recycle(self, container):
         for F in (Ramalan, History, LhatData):
             frame = F(container, self)
 
@@ -34,11 +57,6 @@ class Utama(Tk):
 
             frame.grid(row=0, column=0, sticky="nsew")
 
-        self.show_frame(Ramalan)
-
-    def show_frame(self, cont):
-        frame = self.frames[cont]
-        frame.tkraise()
 
 
 app = Utama()
--- LihatData.py
+++ LihatData.py
@@ -8,8 +8,6 @@ except ImportError:
     from tkinter import *
     import tkinter.ttk as ttk
 
-import History
-import Ramalan
 import Database
 
 
@@ -19,17 +17,6 @@ class LhatData(Frame):
         Frame.__init__(self, parent)
         master = Frame(self)
         master.pack(fill=BOTH, expand=True)
-        menubar = Menu(self)
-        controller.config(menu=menubar)
-        fileMenu = Menu(menubar)
-        fileMenu.add_command(label="Lihat", command=lambda: controller.show_frame(Ramalan.Ramalan))
-        menubar.add_cascade(label="Import", menu=fileMenu)
-        hisMenu = Menu(menubar)
-        hisMenu.add_command(label="Lihat", command=lambda: controller.show_frame(History.History))
-        menubar.add_cascade(label="History", menu=hisMenu)
-        lhtMenu = Menu(menubar)
-        lhtMenu.add_command(label="Lihat Data", command=lambda: controller.show_frame(LhatData))
-        menubar.add_cascade(label="Lihat Data", menu=lhtMenu)
 
         self.judul = Label(master, text="LIST DATA", font="Helvetica 16 bold")
         self.judul.pack(fill=X, ipady=15)
@@ -47,6 +34,3 @@ class LhatData(Frame):
 
     def hapus(self, data):
         Database.HAPUSDATA(data)
-
-
-
